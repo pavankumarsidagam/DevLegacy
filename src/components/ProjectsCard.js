c0868364709e5c266abbe1f585bf06ae6ac58dcf
pavankumarsidagam <pavankumarsidagam1@gmail.com>
2025-01-09 11:24:10 +0530

    New project included in Projects Section

--- src/components/ProjectsCard.js
+++ src/components/ProjectsCard.js
@@ -7,6 +7,12 @@ const ProjectsCard = () => {
         <div className="card bg-white rounded-lg p-6 col-span-1 row-span-1 md:row-span-3 md:col-span-2">
             <h4 className="font-bold card-content border-b-2 mb-2 border-gray-300" >Projects</h4>
             <div className="text-sm card-content-syne max-h-60 overflow-y-hidden hover:overflow-y-auto">
+
+                <div className="flex flex-col pb-2">
+                <span className='text-gray-600'>TMS - Tutor Management System (In Progress)</span>
+                <span className="text-gray-400 text-xs">Developing a Tutor Management System to simplify student management, payment tracking, and communication. Features include user authentication, a dashboard for insights, automated SMS reminders, and a student portal for managing records. Built with React, Node.js, Express.js, and MongoDB, focusing on responsive design and efficiency.</span>
+                </div>
+
                 <div className="flex flex-col pb-2">
                 <span className={textColor}>GME Budget</span>
                 <span className='text-gray-400 text-xs'>Led the design and implementation of a Budget Management System for Ford’s GME team, optimizing cost estimations and headcount forecasts. Collaborated closely with clients, enhancing system performance, streamlining data retrieval, and ensuring accurate budget allocation.</span>
@@ -20,6 +26,11 @@ const ProjectsCard = () => {
                 <div className="flex flex-col pb-2">
                 <span className={textColor}>ReferNext</span>
                 <span className='text-gray-400 text-xs'>Developed innovative solutions to resolve complex technical challenges. Collaborated with the team to troubleshoot deployment issues, apply research-driven improvements, and streamline project execution.</span>
+                </div>
+
+                <div className="flex flex-col pb-2">
+                <span className={textColor}>Resume Portfolio Website</span>
+                <span className="text-gray-400 text-xs">Designed and developed a personal resume website using React, focusing on modular components, responsive design, and modern deployment tools. The project serves as a portfolio showcase.</span>
                 </div>
             </div>
         </div>
